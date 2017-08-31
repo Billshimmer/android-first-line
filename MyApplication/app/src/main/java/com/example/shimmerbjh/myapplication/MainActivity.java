9984484fab62be8c814d0ea197a35c8b78b218a6
shimmer <shimmer.bjh@gmail.com>
2017-08-31 14:16:45 +0800

    五种布局

--- MyApplication/app/src/main/java/com/example/shimmerbjh/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/shimmerbjh/myapplication/MainActivity.java
@@ -21,6 +21,11 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnShow;
+    private Button btnLayout_1;
+    private Button btnLayout_2;
+    private Button btnLayout_3;
+    private Button btnLayout_4;
+    private Button btnLayout_5;
     private EditText editText;
     private ImageView imageView;
     private ProgressBar progressBar;
@@ -31,12 +36,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnShow = (Button) findViewById(R.id.button);
+        btnShow = (Button) findViewById(R.id.button);btnShow.setOnClickListener(this);
+        btnLayout_1 = (Button) findViewById(R.id.button_layout_1); btnLayout_1.setOnClickListener(this);
+        btnLayout_2 = (Button) findViewById(R.id.button_layout_2); btnLayout_2.setOnClickListener(this);
+        btnLayout_3 = (Button) findViewById(R.id.button_layout_3); btnLayout_3.setOnClickListener(this);
+        btnLayout_4 = (Button) findViewById(R.id.button_layout_4); btnLayout_4.setOnClickListener(this);
+        btnLayout_5 = (Button) findViewById(R.id.button_layout_5); btnLayout_5.setOnClickListener(this);
         editText = (EditText) findViewById(R.id.edit_text);
         imageView = (ImageView) findViewById(R.id.img);
         progressBar = (ProgressBar) findViewById(R.id.progress_bar);
-
-        btnShow.setOnClickListener(this);
 
         intentFilter = new IntentFilter();
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
@@ -90,28 +98,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v){
         switch (v.getId()){
             case R.id.button:
-//                String text = editText.getText().toString();
-//                Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
-//                imageView.setImageResource(R.drawable.img_2);
-//                progressBar.setProgress(progressBar.getProgress() + 10);
-//                final AlertDialog.Builder dialog = new AlertDialog.Builder (MainActivity.this);
-//                dialog.setTitle("这是一个对话框").setMessage("something need confirm").setCancelable(false);
-//                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialogInterface, int i) {
-//                    }
-//                });
-//                dialog.setNegativeButton("Cancal", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialogInterface, int i) {
-//                    }
-//                });
-//                dialog.show();
+                String text = editText.getText().toString();
+                Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+                imageView.setImageResource(R.drawable.img_2);
+                progressBar.setProgress(progressBar.getProgress() + 10);
+                final AlertDialog.Builder dialog = new AlertDialog.Builder (MainActivity.this);
+                dialog.setTitle("这是一个对话框").setMessage("something need confirm").setCancelable(false);
+                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                dialog.setNegativeButton("Cancal", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                dialog.show();
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.text:
-                imageView.setImageResource(R.drawable.img_1);
+            case R.id.button_layout_1:
+                Intent intent1 = new Intent(MainActivity.this, LinearLayoutActivity.class);
+                startActivity(intent1);
+                break;
+            case R.id.button_layout_2:
+                Intent intent2 = new Intent(MainActivity.this, RelativeActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.button_layout_3:
+                Intent intent3 = new Intent(MainActivity.this, AbsoluteActivity.class);
+                startActivity(intent3);
+                break;
+            case R.id.button_layout_4:
+                Intent intent4 = new Intent(MainActivity.this, FrameActivity.class);
+                startActivity(intent4);
+                break;
+            case R.id.button_layout_5:
+                Intent intent5 = new Intent(MainActivity.this, TableActivity.class);
+                startActivity(intent5);
                 break;
             default:
                 break;
